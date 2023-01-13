11dd35d41fabfc9d6899871aa967748c3d4821dd
Rayane Fassi <rayanefassifihri25@gmail.com>
2023-01-13 21:36:59 +0100

    edit and delete reviews works but not based on user yet

--- client/src/ReviewContainer.js
+++ client/src/ReviewContainer.js
@@ -1,11 +1,80 @@
+import { useState } from "react"
+
 
 function ReviewContainer({review, setReviews, setInput, setRatingInput }){
 
-    console.log(review)
+    const [reviewEdit, setReviewEdit] = useState("")
+    const [renderEdit, setEdit] = useState(false)
+    const [updateComment, setUpdateComment] = useState(review.comment)
+    const [updateRating, setUpdateRating] = useState(review.star_rating)
+
+    const updateReview = (updatedReview) => {
+        setReviews(current => {
+            return current.map(review => {
+             if (review.id === updatedReview.id){
+               return updatedReview
+             } else {
+               return review
+             }
+            })
+          })
+    }
+    const handleUpdate = e => {
+        e.preventDefault()
+        fetch(`/reviews/${review.id}`, {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                comment: updateComment,
+                star_rating: updateRating
+            })
+        })
+        .then ( res => res.json())
+        .then ( newReview => updateReview(newReview))
+    
+        setEdit(!renderEdit)
+    }
+    
+   
+    const deleteReview = id => {
+        setReviews(currentReview => currentReview.filter( review => review.id !== id ))
+    }
+    
+    const handleDelete = () => {
+        fetch(`/reviews/${review.id}`, {
+            method: 'DELETE',
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then( () => { deleteReview(review.id)} )
+    }
+    
     return(
         <>
             <p>{review.comment}</p>
             <p>{review.star_rating}</p>
+            { renderEdit ? (
+                <form onSubmit={handleUpdate}>
+                    <input 
+                        type="text" 
+                        value={updateComment} 
+                        onChange={e => setUpdateComment(e.target.value)} 
+                    />
+                    <input 
+                        type="number" 
+                        min="1" 
+                        max="5" 
+                        value={updateRating} 
+                        onChange={e => setUpdateRating(e.target.value)} 
+                    />
+                    <button type="submit">Save</button>
+                    <button onClick={() => setEdit(!renderEdit)}>Cancel</button>
+                </form>
+            ) : (
+                <>
+                    <button onClick={() => setEdit(!renderEdit)}>Edit</button>
+                    <button onClick={handleDelete}>Delete</button>
+                </>
+            )}
         </>
     )
 }
